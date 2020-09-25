8adfb21466da81c1188dc4c80f28f8c757d885ab
William Banks <williamjb555@gmail.com>
2020-09-25 16:22:32 +0100

    Fixed some code style things

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,14 @@
 #include <QApplication>
 #include "Application.h"
 
-int main(int argc, char *argv[])
-{
-    QApplication app(argc, argv);
+int main(int argc, char *argv[]) {
+	QApplication app(argc, argv);
 	app.setApplicationName(QObject::tr("Naughts and Crosses"));
 	app.setApplicationDisplayName(QObject::tr("Naughts and Crosses"));
-    Ext_NaughtsAndCrosses w;
+	Ext_NaughtsAndCrosses w;
 
 	Application::Instance(); // Create instance of Application
 
-    w.show();
-    return app.exec();
+	w.show();
+	return app.exec();
 }
